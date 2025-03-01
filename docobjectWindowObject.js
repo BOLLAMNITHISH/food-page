f305fc81ba0eb5a9ae52f50d517c534c04d05add
BOLLAMNITHISH <166913324+BOLLAMNITHISH@users.noreply.github.com>
2025-03-01 18:49:39 +0530

    adding

---
+++ docobjectWindowObject.js
@@ -0,0 +1,6 @@
+console.log(window.document);
+localStorage.setItem("HEllO","HII this is Javascript")
+console.log(window.localStorage);
+console.log(window.cookie);
+console.log(window.console);
+console.log(window.close);
---
+++ dommanipulaton.js
@@ -0,0 +1,9 @@
+const h1=document.querySelector("h1");
+const button=document.querySelector(button);
+/* button.addEventListener("Click", () => {
+        h1.innerText = 'Hello ${Math.floor{Math.random()*9}';
+    }); */
+    setInterval(function (){
+        h1.innerText = 'Hello ${Math.floor{Math.random()*99}';
+    }, 1000);
+
